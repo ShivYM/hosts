ecb68aca33f6bdf1cbadb2ff7dc822538b4f6e1b
Shivkumar <shivkumar@Shivkumars-MacBook-Pro.local>
2020-12-04 18:19:40 +0530

    mod 3

--- customJS.js
+++ customJS.js
@@ -4,22 +4,50 @@ window.addEventListener('message', function(eventData) {
     try { 
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
-             if (event.event_code === "custom-event" && event.data && event.data.code === "otp") {
-                jQuery.post('/cart/add.js', {
-                    items: event.data.data
-                });
-                return;
-            }
-            else if (event.event_code === "custom-event" && event.data && event.data.code === "submitClaim") {
-                jQuery.post('/cart/update.js', {
-                  updates:event.data.data
-                });
-                return;
-            }
-            else{
-                return;
+            //  if (event.event_code === "custom-event" && event.data && event.data.code === "otp") {
+            //     jQuery.post('/cart/add.js', {
+            //         items: event.data.data
+            //     });
+            //     return;
+            // }
+            // else if (event.event_code === "custom-event" && event.data && event.data.code === "submitClaim") {
+            //     jQuery.post('/cart/update.js', {
+            //       updates:event.data.data
+            //     });
+            //     return;
+            // }
+            // else{
+            //     return;
+            // }
+
+            if (event.event_code === 'custom-event') {
+                iframe = document.getElementById('ymIframeId');
+                var eventData = event.data.data;
+                console.log(eventData);
+                switch (event.data.code) {
+                    case "otp":
+                        console.log("Within event block");
+                        console.log(event.data)
+                        // var myWindow = window.open(eventData.url, "new window", "height=500, width=500, top=400, left=400");
+                        return;
+                    case "pdf_download":
+                        var name = eventData.doc_name;
+                        var byteArray = convertToByteArray(eventData.body);
+                        saveByteArray([byteArray], name + ".pdf");
+                        return;
+                    case "multiple_pdfs_download":
+                        for (let index = 0; index < eventData.body.length; index++) {
+                            var name = eventData.body[index].doc_name;
+                            var byteArray = convertToByteArray(eventData.body[index].body);
+                            saveByteArray([byteArray], name + ".pdf");    
+                        }
+                    default:
+                        console.log('No event handler defined for ' + event.data.code);
+                        return;
+                }
             }
          }
+         
     } catch (error) {
         return;
     }
