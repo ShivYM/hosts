5a8186fb817d32b36c78f6441552595338766561
Shivkumar <shivkumar@Shivkumars-MacBook-Pro.local>
2020-12-07 12:04:41 +0530

    iframe count

--- customJS.js
+++ customJS.js
@@ -31,15 +31,23 @@ window.addEventListener('message', function(eventData) {
                         console.log(event.data)
                         // var myWindow = window.open(event.data.data, "new window", "height=500, width=500, top=400, left=400");
                         // window.top.postMessage('hello', '*')
-                        let myiframe = document.getElementByName('ymIframe')
+                        // let myiframe = document.getElementByName('ymIframe')
                         // myiframe.contentWindow.postMessage('hello', '*');
                         
+
+                        var sourceFrame = null; // this is the IFRAME which send the postMessage
+                        var myFrames = document.getElementsByTagName("IFRAME");
+                        // detect the source for IFRAMEs with same-origin URL
+                        for (var i=0; i<myFrames.length; i++) {
+                            console.log("Count IFRAME", i);
+                        }
+
                         
-                        setTimeout(() => {
-                            // window.top.postMessage('hello', '*');    
-                            myiframe.postMessage('hello', '*');
-                            myiframe.contentWindow.postMessage('otp', '*');
-                        }, 2000);
+                        // setTimeout(() => {
+                        //     // window.top.postMessage('hello', '*');    
+                        //     myiframe.postMessage('hello', '*');
+                        //     myiframe.contentWindow.postMessage('otp', '*');
+                        // }, 2000);
                         
                         return;
                     case "pdf_download":
