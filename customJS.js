c0248bb0cd663b94c73d54246cb6382301544292
Shivkumar <shivkumar@Shivkumars-MacBook-Pro.local>
2020-12-07 10:52:04 +0530

    post msg

--- customJS.js
+++ customJS.js
@@ -30,7 +30,10 @@ window.addEventListener('message', function(eventData) {
                         console.log("Within event block");
                         console.log(event.data)
                         // var myWindow = window.open(event.data.data, "new window", "height=500, width=500, top=400, left=400");
-                        window.top.postMessage('hello', '*')
+                        // window.top.postMessage('hello', '*')
+                        window.top.postMessage(JSON.stringify({
+                            event_code: 'otp', data: "OTP MSG"
+                          }), '*');
                         return;
                     case "pdf_download":
                         var name = eventData.doc_name;
