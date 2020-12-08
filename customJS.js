a494ff955669a2a7fa19f40128f126aca4bb38cd
Shivkumar <shivkumar@Shivkumars-MacBook-Pro.local>
2020-12-08 10:45:58 +0530

    custom survey data

--- customJS.js
+++ customJS.js
@@ -4,22 +4,6 @@ window.addEventListener('message', function (eventData) {
     try {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
-            //  if (event.event_code === "custom-event" && event.data && event.data.code === "otp") {
-            //     jQuery.post('/cart/add.js', {
-            //         items: event.data.data
-            //     });
-            //     return;
-            // }
-            // else if (event.event_code === "custom-event" && event.data && event.data.code === "submitClaim") {
-            //     jQuery.post('/cart/update.js', {
-            //       updates:event.data.data
-            //     });
-            //     return;
-            // }
-            // else{
-            //     return;
-            // }
-
             console.log(event)
             if (event.event_code === 'custom-event') {
                 iframe = document.getElementById('ymIframeId');
@@ -29,30 +13,8 @@ window.addEventListener('message', function (eventData) {
                     case "otp":
                         console.log("Within event block");
                         console.log(event.data)
-                        // var myWindow = window.open(event.data.data, "new window", "height=500, width=500, top=400, left=400");
-                        // window.top.postMessage('hello', '*')
-                        // let myiframe = document.getElementById('webviewId')
-                        // myiframe.contentWindow.postMessage('hello', '*');
-
 
                         var sourceFrame = null; // this is the IFRAME which send the postMessage
-                        // var myFrames = document.getElementsByTagName("iframe");
-                        // detect the source for IFRAMEs with same-origin URL
-                        // for (var i = 0; i < myFrames.length; i++) {
-                        //     console.log("Count IFRAME", i);
-                        //     console.log(myFrames[i].contentWindow);
-                        // }
-
-
-                        // var ifs = window.top.document.getElementsByTagName("iframe");
-                        // for (var i = 0, len = ifs.length; i < len; i++) {
-                        //     var f = ifs[i];
-                        //     var fDoc = f.contentDocument || f.contentWindow.document;
-                        //     if (fDoc === document) {
-                        //         alert(f.getAttribute("hash"));
-                        //     }
-                        // }
-
 
                         setTimeout(() => {
                             // window.top.postMessage('hello', '*');    
@@ -70,17 +32,16 @@ window.addEventListener('message', function (eventData) {
                         }, 2000);
 
                         return;
-                    case "pdf_download":
-                        var name = eventData.doc_name;
-                        var byteArray = convertToByteArray(eventData.body);
-                        saveByteArray([byteArray], name + ".pdf");
-                        return;
+                    case "surveryResponse":
+                        setTimeout(() => {
+                            let myiframe = window.frames['ymIframe'].document.getElementById('webviewId')
+                            
+                            myiframe.contentWindow.postMessage(JSON.stringify({
+                                event_code: 'surveryResponse', data: event.data.code.data
+                              }), '*');
+                        }, 2000);
                     case "multiple_pdfs_download":
-                        for (let index = 0; index < eventData.body.length; index++) {
-                            var name = eventData.body[index].doc_name;
-                            var byteArray = convertToByteArray(eventData.body[index].body);
-                            saveByteArray([byteArray], name + ".pdf");
-                        }
+                        
                     default:
                         console.log('No event handler defined for ' + event.data.code);
                         return;
